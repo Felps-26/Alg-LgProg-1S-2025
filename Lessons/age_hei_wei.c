8760324146473eaf43727e4447279bc72e222084
Felps-26 <filipiramos4152@gmail.com>
2025-03-28 11:05:50 -0300

    ,

--- Lessons/age_hei_wei.c
+++ Lessons/age_hei_wei.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
-int main(){
+int main(void){
 
     int age;
     printf("Hello user, tell me your age:\n");
     printf("age:");
     scanf("%d", &age);
     getchar(); 
+
+
     float wei;
     printf("Tell me your weigth:\n");
     printf("weigth:");
     scanf("%f", &wei);
     getchar();
+
+
     float hei;
     printf("Tell me your height:\n");
     printf("height:");
@@ -19,8 +23,8 @@ int main(){
     getchar();
     printf("\nUser, you are %d years old, you have %.2fkg, and you are %.2fm tall\n", age, wei, hei);
 
+
     float double_wei = wei * 2.0;
     printf("double the weight is %.2f", double_wei);
     
-    return 0;
 }
