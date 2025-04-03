455a212d42d7694fae7efc630d2976388dcde128
Felps-26 <filipiramos4152@gmail.com>
2025-04-03 12:54:06 -0300

    .

--- Atividade2/area.c
+++ Atividade2/area.c
@@ -20,7 +20,7 @@
 //
 //        area = pi * (raid * raid);
 //
-//     printf("This circumference area measures %.2f area units\n", area);
+//      printf("This circumference area measures %.2f area units\n", area);
 //    }
 //
 //    if(option == 2){
@@ -105,3 +105,89 @@
 //    return 0;
 //}
 
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    int option;
+
+    printf("choose an area to calculate:\n1-cincumference \n2-rectangle \n3-rhobus \n4-square \n5-trapeze \n6-triangle");
+    scanf("%d", &option);
+
+    switch (option)
+    {
+    case '1':
+        float area, raid, pi = 3.14;
+        printf("User, use this program to calculate a circunference area!");
+        printf("\nTell me the circunference radius: ");
+        scanf("%f", &raid);
+        area = pi * (raid * raid);
+        printf("This circumference area measures %.2f area units\n", area);
+        break;
+
+    case '2':
+       float area, base, hei;
+        printf("User, use this program to calculate a retangle area!\n");
+        printf("Tell me the base and the height measures:\n");
+        printf("Base:");
+        scanf("%f", &base);
+        printf("Height:");
+        scanf("%f", &hei);
+        area = base * hei;
+        printf("This retangle area measures: %.2f area units", area);
+        break;
+
+    case '3':
+    float area, Di, di;
+        printf("User, use this program to calculate a rhombus area!\n");
+        printf("Tell me the largest diagonal and the smallest diagonal measures:\n");
+        printf("Largest diagonal:");
+        scanf("%f", &Di);
+        printf("Smallest diagonal:");
+        scanf("%f", &di);   
+        area = (Di * di) / 2;
+        printf("This rhombus area measures: %.2f area units", area);
+        break;
+
+    case '4':
+        float area, side;
+        printf("User, use this program to calculate a square area!\n");
+        printf("Tell me the side measure:\n");
+        scanf("%f", &side);
+        area = side * side;
+        printf("This square area measures: %.2f area units", area);
+        break;
+    
+    case '5':
+        float area, Ba, ba, hei;
+        printf("User, use this program to calculate a trapeze area!\n");
+        printf("tell me the bases and the height measures:\n");
+        printf("Largest base:");
+        scanf("%f", &Ba);
+        printf("Smallest base:");
+        scanf("%f", &ba);
+        printf("Height:");
+        scanf("%f", &hei);
+        area = ((Ba + ba) * hei) / 2;
+        printf("This trapeze area measures: %.2f area units", area);
+        break;
+
+    case '6':
+        float area, base, hei;
+        printf("User, use this program to calculate a triangle area!\n");
+        printf("Tell me the base and the height measures:\n");
+        printf("Base:");
+        scanf("%f", &base);
+        printf("Height:");
+        scanf("%f", &hei);
+        area = (base * hei) / 2;
+        printf("This triangle area measures: %.2f area units", area);
+        break;
+        
+        default:
+        break;
+    }
+
+    return 0;
+}
