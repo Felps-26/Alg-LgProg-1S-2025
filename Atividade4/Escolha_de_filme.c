012408277a5246d6ae1c61ee1ad641f3aca3f554
Felps-26 <filipiramos4152@gmail.com>
2025-04-04 13:05:39 -0300

    .

---
+++ Atividade4/Escolha_de_filme.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int main(){
+
+int LI;
+    printf("Hello user, do you want to watch:\n1-light film\n2-intense film?\n");
+    printf("Option:");
+    scanf("%d", &LI);
+
+    if(LI == 1){
+        int CA;
+        printf("Some examples of light films genres are:\n1-Comedy\n2-Animations\n");
+        printf("Option:");
+        scanf("%d", &CA);
+
+        if(CA == 1){
+            printf("If you want to whatch some comedy, I'd recomend you to watch As Branquelas!");
+        }
+            
+        if(CA == 2){
+            printf("If you want to watch some animation, I'd recomend you to watch Toy Story!");
+        }
+    }
+
+    if(LI == 2){
+        int HA;
+        printf("Some examples of intese films genres are:\n1-Horror\n2-Action\n");
+        printf("Option:");
+        scanf("%d", &HA);
+        
+        if(HA == 1){
+            int PS;
+            printf("You want to watch some:\n1-Psiocological Horror\n2-Supernatural Horror\n");
+            printf("Option:");
+            scanf("%d", &PS);
+
+            if(PS == 1){
+                printf("If want to watch some Psi.Horror, I'd recomend you to wacth Corra!");
+            }
+            if(PS == 2){
+                printf("If you want ti watch some Sup. Horror, I'd recomend you to watch Invocacao do Mal");
+            }
+
+        }
+        
+        if(HA == 2){
+            int SA;
+            printf("You want to watch some:\n1-Super Heroes\n2-Realistic Adventures\n");
+            printf("Option:");
+            scanf("%d", &SA);
+
+            if(SA == 1){
+                printf("If you want to watch some Sup.Heroes, I'd recomend you to wacth Vingadores Ultimato!");
+            }
+            if(SA == 2){
+                printf("If you want to watch some Rea.Adventures, I'd recomend you to wacth Mad Max!");
+            }
+        }
+
+    }
+    
+    return 0;
+}
