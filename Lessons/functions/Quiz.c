a87c481afed4a0a674d6dcaa6e639321763061f5
Felps-26 <filipiramos4152@gmail.com>
2025-05-15 20:36:11 -0300

    .

--- Lessons/functions/Quiz.c
+++ Lessons/functions/Quiz.c
@@ -1,31 +1,63 @@
 #include <stdio.h>
 
-int read_valid_value(int option1, int option2, char* option1_text, char* option2_text, char* question){
-    int valid_option;
+int Start_QUIZ(int option1, int option2, char* option1_text, char* option2_text, char* question){
+    int valid_option1;
 
     do{
-        printf("%s\nChoose an option: \n%d - %s\n\n%d - %s\n",question, option1, option1_text, option2, option2_text);
-        scanf("%d", &valid_option);
-    }while(valid_option != option1 && valid_option != option2);
+        printf("%s\nChoose an option: \n%d - %s\n%d - %s\n",question, option1, option1_text, option2, option2_text);
+        scanf("%d", &valid_option1);
 
-    return valid_option;
+        if(valid_option1 != option1 && valid_option1 != option2){
+            printf("Invalid option! Please Try again.\n");
+        }
+    }while(valid_option1 != option1 && valid_option1 != option2);
+
+    return valid_option1;
+}
+
+int read_valid_value(char* question, int option1, int option2, int option3, int option4, char* option1_text, char* option2_text, char* option3_text, char* option4_text){
+    int valid_option2;
+
+    do{
+        printf("%s\nChoose an option: \n%d - %s\n%d - %s\n%d - %s\n%d - %s\n",question, option1, option1_text, option2, option2_text, option3, option3_text, option4, option4_text);
+        scanf("%d", &valid_option2);
+
+        if(valid_option2 != option1 && valid_option2 != option2 && valid_option2 != option3 && valid_option2 != option4){
+            printf("Invalid option! Please Try again.\n");
+        }
+    }while(valid_option2 != option1 && valid_option2 != option2 && valid_option2 != option3 && valid_option2 != option4);
+
+    return valid_option2;
 }
 
 int main(){
 
-    int option, awnser;
+    int option, answer;
 
-    option = read_valid_value(1, 0, "Start QUIZ", "Exit QUIZ", "Do you want to start the quiz?");
+    option = Start_QUIZ(1, 0, "Start QUIZ", "Exit QUIZ", "Do you want to start the quiz?");
 
     if(option == 1){
 
-        awnser = read_valid_value(1, 2, "Rio de Janeiro", "Brasilia", "what is the capital of Brazil?");
+        answer = read_valid_value(
+            "\n\nWhat is the capital of Brazil?",
+            1, 2, 3, 4,
+            "Rio de Janeiro", "Brasilia", "Salvador", "Sao Paulo"
+        );
 
-        if (awnser == 2){
+        if (answer == 2){
             printf("Correct answer!\n");
         }else{
             printf("Wrong answer!\n");
         }
+
+        answer = read_valid_value(
+            "\n\nWhat is the capital of France?",
+            1, 2, 3, 4,
+            "Paris", "London", "Berlin", "Madrid"
+        );
+    } else{
+        printf("Exiting the quiz...\n");
+        printf("Please try our QUIZ at next time\n");
     }
 
     return 0;
