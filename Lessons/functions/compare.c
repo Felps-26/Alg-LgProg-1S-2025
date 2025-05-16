26299b9c1bb69fccdd12ff8d76ed3b53de1947da
Felps-26 <filipiramos4152@gmail.com>
2025-05-16 12:03:49 -0300

    .

--- Lessons/functions/compare.c
+++ Lessons/functions/compare.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "numbers_library.c"
 
-int main(){
-    int a, b;
-    printf("Enter two numbers: ");
-    scanf("%d %d", &a, &b);
+int main(int argc, char *argv[]) {
+    int a, b, c;
 
-    printf("The bigger value is: %d\n", return_bigger_value(a, b));
-    printf("The smaller value is: %d\n", return_smaller_value(a, b));
+    if(argc != 3 && argc != 4) {
+        printf("Enter 2 or 3 numbers to compare.\n");
+        return 1;
+    } else if(argc == 3) {
+        a = atoi(argv[1]);
+        b = atoi(argv[2]);
+        printf("The bigger value is: %d\n", return_bigger_value(a, b));
+        printf("The smaller value is: %d\n", return_smaller_value(a, b));
+    } else {
+        a = atoi(argv[1]);
+        b = atoi(argv[2]);
+        c = atoi(argv[3]);
+        printf("The biggest value is: %d\n", return_bigger_value_3n(a, b, c));
+        printf("The smallest value is: %d\n", return_smaller_value3n(a, b, c));
+    }
+
+    
 
     return 0;
 }
--- Lessons/functions/numbers_library.c
+++ Lessons/functions/numbers_library.c
@@ -8,10 +8,19 @@ int return_bigger_value(int a, int b){
     }
 }
 
+int return_bigger_value_3n(int a, int b, int c){
+    int bigger = return_bigger_value(a, b);
+    return return_bigger_value(return_bigger_value(a, b),c);
+}
+
 int return_smaller_value(int a, int b){
     if(a < b){
         return a;
     } else{
         return b;
     }
+}
+int return_smaller_value_3n(int a, int b, int c){
+    int smaller = return_smaller_value(a, b);
+    return return_smaller_value(return_smaller_value(a, b),c);
 }
