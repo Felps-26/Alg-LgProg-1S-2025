d9f34d1bd9435ead830dd0437216977a2d46d9f4
Felps-26 <filipiramos4152@gmail.com>
2025-06-05 12:02:26 -0300

    .

---
+++ Lessons/Ponteiros/first.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+
+
+//int main(){
+    //int x = 3;
+//
+    //while (x < 5){
+    //    if(x % 2 == 0){
+    //        x = x + 2;
+    //    } else{
+    //        x--;
+    //    }
+    //}
+//
+    //printf("x = %d\n", x);
+//
+    //return 0;
+
+//}
+
+#define MAX 255 
+
+//global variable
+int y = 1;
+
+void incrementX(int *x){
+        while (*x < 5){
+    
+            *x = 11;
+            y++;
+        }
+    }
+
+    int main(){
+
+        int z = 1;
+        int w = 2;
+
+        incrementX(&z);
+
+        printf("z = %d, y = %d\n", z, y);
+
+        printf("z = %d\n", &z);
+        printf("w = %d\n", &w);
+
+    return 0;
+
+    }
